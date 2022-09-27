acbffd6acf9ebdf8ab6e0d83b12f523ca243d7eb
randychann <randy.chan.2020@scis.smu.edu.sg>
2022-09-27 16:21:17 +0800

    Organized files, created new pages

---
+++ Resources/NAVBAR_CODE_HR.md
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../home-HR.html">
+            <img src="../Resources/Images/logo.png" alt="logo" width="120" height="55" class="d-inline-block align-text-middle">   
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" style="font-size: 20px !important;" aria-current="page" href="../home-HR.html">Home</a>
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex">
+            <a class="btn btn-outline-primary btn-md" href="login.html" role="button">Logout</a>
+        </div> 
+    </div>
+</nav>
+
+<div>
+    <h5 style="margin-left: 30px">Welcome, &lt;Name&gt;, <i>HR Manager</i></h5>
+</div>
--- skills_sample.sql
+++ skills_sample.sql
@@ -11,28 +11,28 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `is212_example`
+-- Database: 'is212_example'
 --
-CREATE DATABASE IF NOT EXISTS `spmProj` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `spmProj`;
+CREATE DATABASE IF NOT EXISTS 'spmProj' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE 'spmProj';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `skill`
+-- Table structure for table 'skill'
 --
 
-CREATE TABLE `skill` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `description`  varchar(100)  DEFAULT NULL
+CREATE TABLE 'skill' (
+  'id' int NOT NULL AUTO INCREMENT,
+  'name' varchar(50) DEFAULT NULL UNIQUE,
+  'description'  varchar(100)  DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `skill`
+-- Dumping data for table 'skill'
 --
 
-INSERT INTO `skill` (`id`, `name`, `description`) VALUES
-(1, 'Communication', 'Learn to ommunicate Well in a team.' ),
-(2, 'Leadership', 'Learn to lead the team well'),
-(3, 'Project Management', 'Learn to manage projects well');
+INSERT INTO 'skill' ('name', 'description') VALUES
+('Communication', 'Learn to ommunicate Well in a team.' ),
+('Leadership', 'Learn to lead the team well'),
+('Project Management', 'Learn to manage projects well');
