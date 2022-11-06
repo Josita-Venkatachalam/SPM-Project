cd2c049101cdc86c46381795c0ff41800f358b87
Sanofer11 <sanofer.2020@scis.smu.edu.sg>
2022-11-06 23:12:08 +0800

    minor chnges to db.sql for prez

--- database.sql
+++ database.sql
@@ -181,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `LearningJourney` (
 ENGINE = InnoDB;
 
 INSERT INTO `LearningJourney` ( `Completion_Status`,`Roles_id`,`Staff_ID`) VALUES
-('In progress', 1, 130001),
+('In progress', 2, 130001),
 ('In progress', 2, 140001),
 ('In progress', 1, 140001),
 ('In progress', 3, 130002),
